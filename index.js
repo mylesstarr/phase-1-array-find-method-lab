55668b626091c8fccb56ee5bd630b9893fa22c3b
Myles Starr <mylesstarr@gmail.com>
2021-04-28 20:42:31 -0400

    Compled assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+   
+  ]
+
+
+  function superbowlWin (array) {
+    const win = array.find(obj => {return obj.result === 'W'})
+    if (win != undefined) {
+        return win.year
+    } else {
+        return undefined
+    }
+}
+
+
+// function resultFunction(array) {
+//     if (array.result == "W") {
+//         console.log(array.year)
+//     }
+    
+// }
+
+// const result = function(array) {
+//     return array.indexof("W")
+// }
+
+
+
+// function superbowlWin(array){
+//     array.find(result)
+// }
+
+
+
 
